17312baa041938a9e86d8669124bf4188e9935f7
Dhrumil Shah <dhrumilshah71011@gmail.com>
2026-03-31 13:38:48 +0530

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Navbar } from "@/components/Navbar";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,6 +29,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="flex min-h-full flex-col">
+        <Navbar />
         <div className="flex min-h-0 flex-1 flex-col">{children}</div>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ import { HomeSections } from "@/components/HomeSections";
 export default function Home() {
   return (
     <div className="flex w-full flex-col bg-zinc-50 font-sans dark:bg-black">
-      <section className="h-[100svh] min-h-[100svh] w-full overflow-hidden">
-        <BlenderScene className="h-[100svh] min-h-[100svh] w-full" />
+      <section className="h-[calc(100svh-3.5rem)] min-h-[calc(100svh-3.5rem)] w-full overflow-hidden">
+        <BlenderScene className="h-[calc(100svh-3.5rem)] min-h-[calc(100svh-3.5rem)] w-full" />
       </section>
       <HomeSections />
     </div>
